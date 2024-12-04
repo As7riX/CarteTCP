8bb0378f0d777fed95456af06011928dc2762c58
As7riX <garibaldialessio06@gmail.com>
2024-12-04 10:58:49 +0100

    Aggiunta Classe Network

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -1,3 +1,5 @@
+import connection.Network;
+
 import java.io.*;
 import java.net.*;
 
@@ -7,29 +9,22 @@ public class Client {
         String serverAddress = "127.0.0.1";
         int port = 12345;
 
-        // Create a socket to connect to the server
-        try (Socket socket = new Socket(serverAddress, port);
-             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-             BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))
-        ) {
-            System.out.println("Connected to server at " + serverAddress + ":" + port);
+        BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
 
-            String userInput;
-            while ((userInput = stdIn.readLine()) != null) {
-                // Send user input to the server
-                out.println(userInput);
+        Network net = new Network();
 
-                // Read response from the server and print it
-                String response = in.readLine();
-                System.out.println("Server response: " + response);
-            }
-        } catch (UnknownHostException e) {
-            System.err.println("Don't know about host " + serverAddress);
-            System.exit(1);
-        } catch (IOException e) {
-            System.err.println("Couldn't get I/O for the connection to " + serverAddress);
-            System.exit(1);
+        int ris = net.start(serverAddress, port);
+
+        System.out.println("Connected to server at " + serverAddress + ":" + port);
+
+        String userInput;
+        while ((userInput = stdIn.readLine()) != null) {
+            // Send user input to the server
+            net.send(userInput);
+
+            // Read response from the server and print it
+            String response = net.recive();
+            System.out.println("Server response: " + response);
         }
     }
 }
---
+++ Client/src/connection/Network.java
@@ -0,0 +1,49 @@
+package connection;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Network {
+
+    private Socket socket;
+    private BufferedReader in;
+    private PrintWriter out;
+
+
+    public Network(){
+
+    }
+
+    public int start(String address, int port){
+        try {
+            socket = new Socket(address, port);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream(), true);
+
+
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host " + address);
+            throw new RuntimeException(e);
+
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to " + address);
+            throw new RuntimeException(e);
+
+        }
+
+        return 0;
+    }
+
+    public void send(String msg){
+        out.println(msg);
+    }
+
+    public String recive() throws IOException {
+        return in.readLine();
+    }
+
+}
